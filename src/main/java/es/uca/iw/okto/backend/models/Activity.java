87cb0d89579cc81d47811497a6de03b6d3b47f85
BorjaRomeroFernandez <1415.romero.borja@gmail.com>
2020-01-10 11:20:57 +0100

    :tada: First commit

---
+++ src/main/java/es/uca/iw/okto/backend/models/Activity.java
@@ -0,0 +1,40 @@
+package es.uca.iw.okto.backend.models;
+
+import java.util.Date;
+import javax.persistence.Entity;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+
+@Entity
+public class Activity extends Service {
+
+    /**
+    *
+    */
+    private static final long serialVersionUID = 1793141522584474874L;
+
+    @Temporal(TemporalType.DATE)
+    private Date start;
+
+    @Temporal(TemporalType.DATE)
+    private Date end;
+
+    public Activity() {
+    }
+
+    public Date getStart() {
+        return start;
+    }
+
+    public void setStart(Date start) {
+        this.start = start;
+    }
+
+    public Date getEnd() {
+        return end;
+    }
+
+    public void setEnd(Date end) {
+        this.end = end;
+    }
+}
---
+++ src/main/java/es/uca/iw/okto/backend/models/OtherService.java
@@ -0,0 +1,15 @@
+package es.uca.iw.okto.backend.models;
+
+import javax.persistence.Entity;
+
+@Entity
+public class OtherService extends Service {
+
+    /**
+     *
+     */
+    private static final long serialVersionUID = 1L;
+
+    public OtherService() {
+    }
+}
