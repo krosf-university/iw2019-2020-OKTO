603423c97036a9a8cdb61a3dba4e73aa64c86292
frpericacho <frpericacho@gmail.com>
2020-01-24 22:33:11 +0100

    Added DataView at Manager with a grid

--- src/main/java/es/uca/iw/okto/views/manager/dashboard/DataView.java
+++ src/main/java/es/uca/iw/okto/views/manager/dashboard/DataView.java
@@ -1,13 +1,16 @@
 package es.uca.iw.okto.views.manager.dashboard;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import com.vaadin.flow.component.dependency.CssImport;
+import com.vaadin.flow.component.grid.Grid;
 import com.vaadin.flow.component.html.H2;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 import com.vaadin.flow.router.PageTitle;
 import com.vaadin.flow.router.Route;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.vaadin.crudui.crud.impl.GridCrud;
 
 import es.uca.iw.okto.backend.models.Activity;
 import es.uca.iw.okto.backend.models.Tour;
@@ -23,24 +26,27 @@ public class DataView extends VerticalLayout {
 
   @Autowired
   public DataView(ActivityService activityService, TourService tourService) {
-    GridCrud<Activity> crud = new GridCrud<>(Activity.class);
     H2 titulo1 = new H2("Activities");
     add(titulo1);
-    crud.getGrid().setColumns("name", "description");
-    crud.getGrid().setColumnReorderingAllowed(true);
-    setSizeFull();
-    add(crud);
-    crud.setOperations(() -> activityService.findAll(), null, null, null);
+    
+    List<Activity> list = new ArrayList<>();
+    list.addAll(activityService.findAll());
+    Grid<Activity> grid = new Grid<>(Activity.class);
+    grid.setItems(list);
+    grid.setColumns("name","description","start","end");
+    add(grid);
 
-    GridCrud<Tour> crud2 = new GridCrud<>(Tour.class);
+
     H2 titulo2 = new H2("Tours");
     add(titulo2);
-    crud2.getGrid().setColumns("name", "description", "price");
-    crud2.getGrid().addColumn(tour -> tour.getCity().getName()).setHeader("City").setKey("tour");
-    crud2.getGrid().setColumnReorderingAllowed(true);
-    setSizeFull();
-    add(crud2);
-    crud2.setOperations(() -> tourService.findAll(), null, null, null);
+
+    List<Tour> list2 = new ArrayList<>();
+    list2.addAll(tourService.findAll());
+    Grid<Tour> grid2 = new Grid<>(Tour.class);
+    grid2.setItems(list2);
+    grid2.setColumns("name","description","start","end","price");
+    grid2.addColumn(tour -> tour.getCity().getName()).setHeader("City");
+    add(grid2);
   }
 
 }
