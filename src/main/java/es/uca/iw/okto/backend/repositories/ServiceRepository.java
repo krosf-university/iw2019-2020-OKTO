a9faf9119b7c293b803e5a67334e51445c9274f8
KROSF <rodrigosanabria22@gmail.com>
2020-01-29 01:31:37 +0100

    fix: query to get servies is working

--- src/main/java/es/uca/iw/okto/backend/repositories/ServiceRepository.java
+++ src/main/java/es/uca/iw/okto/backend/repositories/ServiceRepository.java
@@ -1,6 +1,11 @@
 package es.uca.iw.okto.backend.repositories;
 
+import java.time.LocalDate;
+import java.util.Collection;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import es.uca.iw.okto.backend.models.Service;
 
@@ -9,6 +14,12 @@ import es.uca.iw.okto.backend.models.Service;
  */
 public interface ServiceRepository extends JpaRepository<Service, Long> {
 
-	Service findByNameIgnoreCase(String name);
+  Service findByNameIgnoreCase(String name);
 
+  @Query("SELECT ser FROM Service ser WHERE ser.ship.id IN (SELECT ship.id FROM Trip tr WHERE tr.id IN (SELECT trip.id FROM UserTrip us WHERE us.user.id = :userId) AND tr.end > :now AND :now > tr.start)")
+  Collection<Service> findByUserOnCurrentTrip(@Param("userId") Long id, @Param("now") LocalDate now);
+
+  default Collection<Service> findByUserOnCurrentTrip(Long id) {
+    return findByUserOnCurrentTrip(id, LocalDate.now());
+  }
 }
--- src/main/java/es/uca/iw/okto/views/user/tripservices/TripServicesView.java
+++ src/main/java/es/uca/iw/okto/views/user/tripservices/TripServicesView.java
@@ -1,13 +1,11 @@
 package es.uca.iw.okto.views.user.tripservices;
 
 import java.io.Serializable;
+import java.util.Collection;
 
 import com.vaadin.flow.component.dependency.CssImport;
 import com.vaadin.flow.component.grid.Grid;
-import com.vaadin.flow.component.grid.GridVariant;
 import com.vaadin.flow.component.html.Div;
-import com.vaadin.flow.component.html.H3;
-import com.vaadin.flow.data.renderer.ComponentRenderer;
 import com.vaadin.flow.router.AfterNavigationEvent;
 import com.vaadin.flow.router.AfterNavigationObserver;
 import com.vaadin.flow.router.PageTitle;
@@ -16,41 +14,49 @@ import com.vaadin.flow.router.Route;
 import org.springframework.beans.factory.annotation.Autowired;
 
 import es.uca.iw.okto.backend.HasLogger;
+import es.uca.iw.okto.backend.models.Service;
 import es.uca.iw.okto.backend.models.Trip;
+import es.uca.iw.okto.backend.repositories.ServiceRepository;
 import es.uca.iw.okto.backend.security.CurrentUser;
-import es.uca.iw.okto.backend.services.TripService;
 import es.uca.iw.okto.views.MainView;
 
 @Route(value = "user/services", layout = MainView.class)
 @PageTitle("Trips")
 @CssImport("./styles/views/users/user-services.css")
 public class TripServicesView extends Div implements HasLogger, AfterNavigationObserver, Serializable {
-    private static final long serialVersionUID = 4405988136982821755L;
+  private static final long serialVersionUID = 4405988136982821755L;
 
-    private final Grid<Trip> grid;
-    @Autowired
-    private TripService tripService;
+  private final Grid<Trip> grid;
+  @Autowired
+  private ServiceRepository serviceRepository;
 
-    @Autowired
-    private CurrentUser currentUser;
+  @Autowired
+  private CurrentUser currentUser;
 
-    public TripServicesView() {
-        setId("user-services-view");
-        grid = new Grid<>();
-        grid.setId("list");
-        grid.addThemeVariants(GridVariant.LUMO_NO_BORDER, GridVariant.LUMO_NO_ROW_BORDERS);
-        grid.setHeightFull();
-        grid.addColumn(new ComponentRenderer<>(trip -> {
-            H3 h3 = new H3(trip.getName());
-            Div div = new Div(h3);
-            div.addClassName("user-services");
-            return div;
-        }));
-        add(grid);
-    }
+  public TripServicesView() {
+    setId("user-services-view");
+    grid = new Grid<>();
+    // grid.setId("list");
+    // grid.addThemeVariants(GridVariant.LUMO_NO_BORDER,
+    // GridVariant.LUMO_NO_ROW_BORDERS);
+    // grid.setHeightFull();
+    // grid.addColumn(new ComponentRenderer<>(trip -> {
+    // H3 h3 = new H3(trip.getName());
+    // Div div = new Div(h3);
+    // div.addClassName("user-services");
+    // return div;
+    // }));
+    add(grid);
+  }
 
-    @Override
-    public void afterNavigation(AfterNavigationEvent event) {
-        grid.setItems(tripService.findByUser(currentUser.getUser()));
-    }
+  @Override
+  public void afterNavigation(AfterNavigationEvent event) {
+    // grid.setItems(tripService.findByUser(currentUser.getUser()));
+
+    Collection<Service> services = serviceRepository.findByUserOnCurrentTrip(currentUser.getUser().getId());
+
+    services.forEach(s -> {
+      getLogger().warn(s.getId().toString());
+    });
+  }
 }
